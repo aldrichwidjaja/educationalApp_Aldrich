5a1b120242f2a5bc017b9ce59f1639144d09ef24
Aldrich Widjaja <50089724+aldrichwidjaja@users.noreply.github.com>
2020-01-10 19:19:17 +0800

    ADDED SONG

--- EducationalApp_Aldrich/app/src/main/java/com/example/educationalapp_aldrich/MainActivity.java
+++ EducationalApp_Aldrich/app/src/main/java/com/example/educationalapp_aldrich/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.educationalapp_aldrich;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,6 +19,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MediaPlayer ring= MediaPlayer.create(getApplicationContext(),R.raw.awesomeness);
+        ring.setLooping(true);
+        ring.start();
 
         play = (Button) findViewById(R.id.play);
         play.setOnClickListener(new View.OnClickListener() {
